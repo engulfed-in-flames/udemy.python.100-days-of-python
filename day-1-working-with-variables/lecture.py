adf656066bcc50521a9e1ca86ae24bf2cc9bcea9
GyeongSu Gim <recona97@gmail.com>
2024-09-11 11:50:11 +0900

    Complete the course for day 1

---
+++ day-1-working-with-variables/lecture.py
@@ -0,0 +1,15 @@
+# 1. Print
+# print("Hello, Python!")
+# print("Hello, Python!\n")
+# print("Hello," + "Python!")
+# print("Hello," + " " + "Python!")
+
+# 2. Input
+# print("====================")
+# print("Hello, " + input("What is your name?: ") + "!")
+
+# 3. Variables
+# print("====================")
+# username = "Michael"
+# length = len(username)
+# print(length)
---
+++ day-1-working-with-variables/main.py
@@ -0,0 +1,7 @@
+# Band Name Generator
+print("Welcome to the Band Name Generator")
+
+city: str = input("What's the name of the city you grew up in?\n")
+pet: str = input("What's your pet's name?\n")
+
+print(f"Your band name could be: {city} {pet}")
